aef7efeb031952753497f7cca98315fbd628dc12
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2022-09-08 18:45:40 -0400

    Allowed for multiple feed files
    Script now searches through the Feeds directory and sends an email for each JSON file

--- PyPaper.py
+++ PyPaper.py
@@ -3,22 +3,26 @@ import json
 import smtplib
 from email.message import EmailMessage
 
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'Feeds', 'testFeed.json')
-file = open(filename, "r")
-feed = json.load(file)
+# Find all feeds in the ./Feeds directory except for the example feed
+feedDir = os.path.join(os.path.dirname(__file__), "Feeds")
+for filename in os.listdir(feedDir):
+  if filename.endswith(".json") and not filename == "exampleFeed.json":
+    # Open and read feed file
+    file = open(os.path.join(feedDir, filename), "r")
+    feed = json.load(file)
+    file.close()
 
-# Create Email
-msg = EmailMessage()
-msg.set_content("Hello World!")
-msg['Subject'] = "Hello"
-msg['From'] = msg['To'] = feed["login"]["email address"]
+    # Create Email
+    msg = EmailMessage()
+    msg.set_content("Hello World!")
+    msg['Subject'] = "Hello"
+    msg['From'] = msg['To'] = feed["login"]["email address"]
 
-# Create SMTP connection
-s = smtplib.SMTP(feed["login"]["smtp server"], feed["login"]["smtp port"])
-s.starttls()
-s.login(feed["login"]["email address"], feed["login"]["email password"])
+    # Create SMTP connection
+    s = smtplib.SMTP(feed["login"]["smtp server"], feed["login"]["smtp port"])
+    s.starttls()
+    s.login(feed["login"]["email address"], feed["login"]["email password"])
 
-# Send Email
-s.send_message(msg)
-s.quit()
+    # Send Email
+    s.send_message(msg)
+    s.quit()
